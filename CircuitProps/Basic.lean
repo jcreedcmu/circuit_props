634f81bfb3a3267a1fa2962b345507fa5802cb83
Jason Reed <jcreed@gmail.com>
2025-04-13 11:33:36 -0400

    Using typeclasses more

--- CircuitProps/Basic.lean
+++ CircuitProps/Basic.lean
@@ -18,82 +18,56 @@ def Tprop := (t : ℝ) → Prop
 -- If a gate's inputs are stable during [0,s],
 -- then it promises to have an output value
 -- at time s+g
-def Xprop := (t : ℝ) → (s : ℝ) → (g: ℝ) → Prop
+def Xprop := (s : ℝ) → (g: ℝ) → Tprop
 
 def Location := Signal → Tprop
 
-class Andable (X : Type) where
-  And : X → X → X
-
-instance : Andable Tprop where
-  And := λ A B => λ t => A t ∧ B t
-
-instance : Andable Xprop where
-  And := λ A B => λ t s g => A t s g ∧ B t s g
-
--- This notation doesn't have much to do with linear logic,
--- I just wanted something to not conflict with ∧ 
-infixr:35 " ⊗ " => Andable.And
-
-
-class Implable (X : Type) where
-  Impl : X → X → X
-
-instance : Implable Tprop where
-  Impl := λ A B => λ t => A t → B t
-
-instance : Implable Xprop where
-  Impl := λ A B => λ t s g => A t s g → B t s g
-
--- This notation doesn't have much to do with linear logic,
--- I just wanted something to not conflict with →  
-infixr:35 " ⊸ " => Implable.Impl
-
-class Delayable (X : Type) where
-  Delay : ℝ → X → X
-
-instance : Delayable Tprop where
-  Delay := λ u A => λ t => A (t + u)
-
-instance : Delayable Xprop where
-  Delay := λ u A => λ t s g => A (t + u) s g
-
-notation "○" => Delayable.Delay
-
 variable (x y z : Location) (t t1 t2 : ℝ)
 
-
-class Constable (X : Type) where
+class Indexed (X : Type) where
+  And : X → X → X
+  Impl : X → X → X
+  Delay : ℝ → X → X
   Const : Prop → X
-
-instance : Constable Tprop where
-  Const x := λ _ => x
-
-class Forallable (X : Type) where
   Forall : (ℝ → X) → X
 
-instance : Forallable Tprop where
+instance : Indexed Tprop where
+  And := λ A B => λ t => A t ∧ B t
+  Impl := λ A B => λ t => A t → B t
+  Delay := λ u A => λ t => A (t + u)
+  Const x := λ _ => x
   Forall k := λ t => ∀ u, k u t
 
+section use_indexed
+open Indexed
 
-def interval (lim: ℝ) {A : Type} [Delayable A] [Constable A] [Implable A] [Forallable A] (X : A): A := 
-   Forallable.Forall (X := A) (λ u => (Constable.Const (u ≤ lim ∧ u ≥ 0)) ⊸  Delayable.Delay (X := A) u X )
+-- This notation doesn't have much to do with linear logic,
+-- I just wanted something to not conflict with ∧, →  
+infixr:35 " ⊗ " => And
+infixr:35 " ⊸ " => Impl
+
+notation "○" => Delay
+notation "∀" u "," body => Forall (λ u => body)
+
+instance : Coe Tprop Xprop where
+  coe x := λ _ _ => x
+
+def interval (lim: ℝ) {A : Type} [Indexed A] (X : A): A := 
+   ∀ u, (Const (u ≤ lim ∧ u ≥ 0)) ⊸ ○ u X 
 
 notation "□" => interval
 
+instance : Indexed Xprop where
+  And := λ A B => λ s g => A s g ⊗ B s g
+  Impl := λ A B => λ s g => □ s (A s g) ⊸ ○ (s + g) (B s g)
+  Delay := λ u A => λ s g => ○ u (A s g)
+  Const x := λ _ _ => Const x
+  Forall k := λ s g => Forall (λ u => k u s g)
 
-def lifted_imp (A B : Tprop): Tprop := λ t => A t → B t
 
+def nand_ts : Xprop := 
+   (x low ⊸ z high) ⊗
+   (y low ⊸ z high) ⊗ 
+   ((x high ⊗ y high) ⊸ z low)
 
-infixr:30 " ⊸ "  => lifted_imp
-
-def with_delay (d1 d2 : ℝ) (A B : Tprop) : Tprop := 
-    □ d1 A ⊸ ○ (d1 + d2) B 
-
-notation A " ⇒ " "[" d1 "," d2 "]" B =>  with_delay d1 d2 A B
-
-def nand_ts (d1 d2 : ℝ) : Tprop := 
-   (x low ⇒ [d1, d2] z high) ⊗
-   (y low ⇒ [d1, d2] z high) ⊗ 
-   ((x high ⊗ y high) ⇒ [d1, d2] z low)
-
+end use_indexed

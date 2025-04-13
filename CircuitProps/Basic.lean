3b36b79fffbf759fe5dae95566ec235f14c27ca4
Jason Reed <jcreed@gmail.com>
2025-04-13 08:56:32 -0400

    Some notation

--- CircuitProps/Basic.lean
+++ CircuitProps/Basic.lean
@@ -7,7 +7,9 @@ open Signal
 
 def Tprop := ℝ → Prop
 
-variable (Place : Type) (x y z : Place) (t t1 t2 : ℝ) (has : Place → Signal → Tprop)
+def Location := Signal → Tprop
+
+variable (x y z : Location) (t t1 t2 : ℝ)
 
 def delay (off: ℝ) (A : Tprop) : Tprop := λ t =>
    A (t + off)
@@ -19,12 +21,19 @@ def interval (lim: ℝ) (A : Tprop) : Tprop := λ t =>
 
 notation "□" => interval
 
-def with_delay (d1 d2 : ℝ) (A B : Tprop) : Tprop := 
-   λ t => □ d1 A t → ○ (d1 + d2) B t
 
-def nand_ts (d1 d2 : ℝ)  := λ t => 
-   with_delay d1 d2 (has x low) (has z high) t ∧ 
-   with_delay d1 d2 (has y low) (has z high) t ∧ 
-   with_delay d1 d2 (λ u => has x high u ∧ has y high u) (has z low) t 
+def lifted_imp (A B : Tprop): Tprop := λ t => A t → B t
+def lifted_and (A B : Tprop): Tprop := λ t => A t ∧ B t
+
+infixr:35 " ∧' "   => lifted_and
+infixr:30 " →'  "  => lifted_imp
+
+def with_delay (d1 d2 : ℝ) (A B : Tprop) : Tprop := 
+    □ d1 A →' ○ (d1 + d2) B 
+
+def nand_ts (d1 d2 : ℝ) : Tprop := 
+   with_delay d1 d2 (x low) (z high) ∧'
+   with_delay d1 d2 (y low) (z high) ∧' 
+   with_delay d1 d2 (x high ∧' y high) (z low) 
 
 

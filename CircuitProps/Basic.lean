670581773624e5c0cd3e8652fb109911de593009
Jason Reed <jcreed@gmail.com>
2025-04-13 09:38:49 -0400

    This works??

--- CircuitProps/Basic.lean
+++ CircuitProps/Basic.lean
@@ -1,11 +1,25 @@
-import Mathlib
+import Mathlib 
 
 inductive Signal where
  | high
  | low
 open Signal
 
-def Tprop := ℝ → Prop
+def Tprop := (t : ℝ) → Prop
+def Xprop := (t : ℝ) → (s : ℝ) → (g: ℝ) → Prop
+
+
+class Andable (X : Type) where
+  And : X → X → X
+
+
+
+instance : Andable Tprop where
+  And := λ A B => λ t => A t ∧ B t
+
+infixr:35 " ∧ " => Andable.And
+
+
 
 def Location := Signal → Tprop
 
@@ -17,25 +31,23 @@ def delay (off: ℝ) (A : Tprop) : Tprop := λ t =>
 notation "○" => delay
 
 def interval (lim: ℝ) (A : Tprop) : Tprop := λ t =>
-   ∀ u, (u ≤ lim ∧ u ≥ 0) → ○ u A t
+   ∀ u, ( (u ≤ lim) ∧  (u ≥ 0)) → ○ u A t
 
 notation "□" => interval
 
 
 def lifted_imp (A B : Tprop): Tprop := λ t => A t → B t
-def lifted_and (A B : Tprop): Tprop := λ t => A t ∧ B t
 
-infixr:35 " ∧' "   => lifted_and
-infixr:30 " →'  "  => lifted_imp
+
+infixr:30 " ⊸ "  => lifted_imp
 
 def with_delay (d1 d2 : ℝ) (A B : Tprop) : Tprop := 
-    □ d1 A →' ○ (d1 + d2) B 
+    □ d1 A ⊸ ○ (d1 + d2) B 
 
 notation A " ⇒ " "[" d1 "," d2 "]" B =>  with_delay d1 d2 A B
 
 def nand_ts (d1 d2 : ℝ) : Tprop := 
-   (x low ⇒ [d1, d2] z high) ∧'
-   (y low ⇒ [d1, d2] z high) ∧'
-   ((x high ∧' y high) ⇒ [d1, d2] z low)
-
+   (x low ⇒ [d1, d2] z high) ∧ 
+   (y low ⇒ [d1, d2] z high) ∧ 
+   ((x high ∧ y high) ⇒ [d1, d2] z low)
 

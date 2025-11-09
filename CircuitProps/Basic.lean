b3dbbfcf0bf493a9c40505907db58d364d052815
Jason Reed <jcreed@gmail.com>
2025-11-09 09:42:50 -0500

    golf

--- CircuitProps/Basic.lean
+++ CircuitProps/Basic.lean
@@ -106,6 +106,15 @@ theorem delay_and_dist (p : ℝ) (A B : Tprop) : (○[p] (A ∧t B)) = ((○[p] 
   apply propext
   exact ⟨id, id⟩
 
+lemma and4_assoc (A B C D : Tprop) : ((A ∧t B) ∧t C ∧t D) = (A ∧t B ∧t C ∧t D) := by
+  funext t
+  apply propext
+  constructor
+  · intro ⟨⟨a, b⟩, c, d⟩
+    exact ⟨a, b, c, d⟩
+  · intro ⟨a, b, c, d⟩
+    exact ⟨⟨a, b⟩, c, d⟩
+
 /--
 Knowing that `A` was true for the past `p+q` time
 is the same thing as knowing that `A` was true
@@ -166,23 +175,10 @@ theorem latch_stable1b (p q : ℝ) (s r qpos qbar : Location)
 
 theorem latch_stable1 (p q : ℝ≥0) (s r qpos qbar : Location)
     (ℓ : Latch p q s r qpos qbar) (sig : Signal) :
-    ⊧ (□[p + q]○[q] (qpos sig ∧t qbar (neg sig) ∧t s high ∧t r high))
+    ⊧ (□[p + q]○[q] (qpos sig ∧t qbar (neg sig))) ∧t (□[p + q]○[q] (s high ∧t r high))
         →t □[q] (qpos sig ∧t qbar (neg sig)) := by
-  rw [add_comm, prev_concat]
-  refine prev_func _ ?_
+  rw [← prev_and_dist, ← delay_and_dist, add_comm, prev_concat, and4_assoc]
+  refine prev_func q ?_
   exact match sig with
   | high => latch_stable1a p q s r qpos qbar ℓ
   | low  => latch_stable1b p q s r qpos qbar ℓ
-
-theorem latch_stable2 (p q : ℝ≥0) (s r qpos qbar : Location)
-    (ℓ : Latch p q s r qpos qbar) (sig : Signal) :
-    ⊧ (□[p + q]○[q] (qpos sig ∧t qbar (neg sig))) ∧t (□[p + q]○[q] (s high ∧t r high))
-        →t □[q] (qpos sig ∧t qbar (neg sig)) := by
-  intro t ⟨h1, h2⟩
-  refine latch_stable1 p q s r qpos qbar ℓ sig t ?_
-  intro u hu
-  refine ⟨?_, ?_, ?_, ?_⟩
-  · exact h1 u hu |>.1
-  · exact h1 u hu |>.2
-  · exact h2 u hu |>.1
-  · exact h2 u hu |>.2
